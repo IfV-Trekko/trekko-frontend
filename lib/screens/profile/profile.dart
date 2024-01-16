f952b35f2d464f3aa4b2210d11319d2cb61b2e91
davidsteimel <132820861+davidsteimel@users.noreply.github.com>
2024-01-16 18:14:33 +0100

    feat: add profile draft

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/cupertino.dart';
 
-class Profile extends StatefulWidget {
-  const Profile({super.key});
+import '../../app_theme.dart';
 
-  @override
-  State<StatefulWidget> createState() {
-    return _ProfileState();
-  }
-}
+//class Profile extends StatefulWidget {
+//  const Profile({super.key});
 
-class _ProfileState extends State<Profile> {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _ProfileState();
+//   }
+// }
+
+class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-        child: Text('Profile'),
+      child: CustomScrollView(
+        slivers: [
+         CupertinoSliverNavigationBar(
+            largeTitle: Text('Profil'),
+         ),
+         SliverFillRemaining(
+           child: Column(
+             children: [
+               CupertinoListSection.insetGrouped(
+                 margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
+                 additionalDividerMargin: 4,
+                 backgroundColor: AppThemeColors.contrast100,
+                 children: <CupertinoListTile>[
+                   CupertinoListTile.notched(
+                     padding: EdgeInsets.only(left: 16, right: 16),
+                     title: Text('Name', style: AppThemeTextStyles.normal),
+                     additionalInfo: const Text('Dein Name'),
+                   ),
+                   CupertinoListTile.notched(
+                     padding: EdgeInsets.only(left: 16, right: 16),
+                     title: const Text('Name'),
+                     additionalInfo: const Text('Dein Name'),
+                   ),
+                 ],
+               ),
+               CupertinoListSection.insetGrouped(
+                 margin: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                 additionalDividerMargin: 4,
+                 backgroundColor: AppThemeColors.contrast100,
+                 children: <CupertinoListTile>[
+                   CupertinoListTile.notched(
+                     padding: EdgeInsets.only(left: 16, right: 16),
+                     title: Text('Name', style: AppThemeTextStyles.normal),
+                     additionalInfo: const Text('Dein Name'),
+                   ),
+                   CupertinoListTile.notched(
+                     padding: EdgeInsets.only(left: 16, right: 16),
+                     title: const Text('Name'),
+                     additionalInfo: const Text('Dein Name'),
+                   ),
+                 ],
+               ),
+             ],
+           ),
+         ),
+        ],
       ),
     );
   }
