f47616bfdb3a9164738777cc484b78de539b393d
Wakahn <jakob.gruen02@gmail.com>
2024-01-16 17:13:21 +0100

    add journalDetail.dart and base box in journalEntry.dart

--- lib/screens/journal/journalEntry.dart
+++ lib/screens/journal/journalEntry.dart
@@ -1,17 +1,34 @@
+import 'package:flutter/cupertino.dart';
 
-
-class JournalEntry extends StatefulWidget{
-  const JournalEntry({super.key});
-
-  @override
-  State<JournalEntry> createState() {
-    return _JournalEntryState();
-  }
-
-  class _JournalEntryState extends State<_JournalEntryState>
-
+class journalEntry extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-  return Container();
+    return GestureDetector(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: CupertinoColors.white,
+            borderRadius: BorderRadius.circular(6.0),
+            border: Border.all(
+              color: CupertinoColors.systemGrey,
+              width: 1.0,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(), //TODO Children einfügen (Labels)
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  void onPressed(){
+
   }
 }
