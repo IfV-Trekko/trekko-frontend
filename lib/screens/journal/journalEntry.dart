a4cadb4eb5232b4de8aaff80e7e061501b5e511b
Wakahn <jakob.gruen02@gmail.com>
2024-01-23 12:39:13 +0100

    add base structure of a journal entry

--- lib/screens/journal/journalEntry.dart
+++ lib/screens/journal/journalEntry.dart
@@ -3,9 +3,8 @@ import 'package:app_frontend/app_theme.dart';
 import 'package:flutter/cupertino.dart';
 
 class journalEntry extends StatelessWidget {
+  final Stream<Trip> trip;
 
-  late Stream<Trip> trip;
-  
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -15,7 +14,7 @@ class journalEntry extends StatelessWidget {
         child: Container(
           width: double.infinity,
           decoration: BoxDecoration(
-            color: CupertinoColors.white,
+            color: AppThemeColors.contrast0,
             borderRadius: BorderRadius.circular(6.0),
             border: Border.all(
               color: AppThemeColors.contrast500,
@@ -33,6 +32,86 @@ class journalEntry extends StatelessWidget {
 
   journalEntry(this.trip);
 
-  void onPressed(){
+  void onPressed() {}
+}
+
+class tripRow extends StatelessWidget {
+  final Stream<Trip> trip;
+
+  tripRow(this.trip);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Trip>(
+      stream: trip,
+      builder: (BuildContext context, AsyncSnapshot<Trip> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CupertinoActivityIndicator(
+              radius: 20, color: AppThemeColors.contrast500); //TODO durch Text ersetzen?
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(snapshot.data!.startTime.toString()),
+                Text(""), //TODO Dauer und Distanz berechnen
+                Text(snapshot.data!.endTime.toString()),
+              ]);
+        }
+      },
+    );
+  }
+}
+
+class VehicleRow extends StatelessWidget {
+  final Stream<Trip> trip;
+
+  VehicleRow(this.trip);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Trip>(
+      stream: trip,
+      builder: (BuildContext context, AsyncSnapshot<Trip> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CupertinoActivityIndicator(
+              radius: 20, color: AppThemeColors.contrast500); //TODO durch Text ersetzen?
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Container();
+        }
+      },
+    );
+  }
+}
+
+class labelRow extends StatelessWidget{
+  final Stream<Trip> trip;
+
+  labelRow(this.trip);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Trip>(
+      stream: trip,
+      builder: (BuildContext context, AsyncSnapshot<Trip> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CupertinoActivityIndicator(
+              radius: 20, color: AppThemeColors.contrast500); //TODO durch Text ersetzen?
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(),
+              Container(),
+            ],
+          );
+        }
+      },
+    );
   }
 }
