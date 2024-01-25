e1b76b5715c5da92fdb45e0c490603c4e12784dc
davidsteimel <132820861+davidsteimel@users.noreply.github.com>
2024-01-25 19:46:10 +0100

    feat: implement different ways of editing form answers

--- lib/screens/profile/input_text_screen.dart
+++ lib/screens/profile/input_text_screen.dart
@@ -1,34 +1,63 @@
+import 'package:app_backend/model/profile/onboarding_question.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:app_backend/model/profile/question_type.dart';
 import '../../app_theme.dart';
 
-class TextInputPage extends StatelessWidget {
-  final String title;
-  final String currentText;
+class TextInputPage extends StatefulWidget {
+  final OnboardingQuestion question;
 
-  TextInputPage({required this.title, required this.currentText});
+  TextInputPage({
+    required this.question,
+  });
+
+  @override
+  _TextInputPageState createState() => _TextInputPageState();
+}
+
+class _TextInputPageState extends State<TextInputPage> {
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       backgroundColor: AppThemeColors.contrast100,
       navigationBar: CupertinoNavigationBar(
-        middle: Text('Profil'),
+        middle: Text(widget.question.title),
       ),
       child: Center(
-        child: Padding( // TODO: use input component as soon as ready
+        child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0),
-          child: CupertinoTextField(
-            placeholder: title,
-            autofocus: true,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            onSubmitted: (String value) {
-              Navigator.of(context).pop(value);
-            },
-          ),
+          child: _buildContentBasedOnQuestionType(),
         ),
       ),
     );
+  }
+
+  Widget _buildContentBasedOnQuestionType() {
+    switch (widget.question.type) {
+      case QuestionType.select:
+      // Logik für Auswahlfragen
+        break;
+
+      case QuestionType.number:
+      // Logik für Zahlenfragen
+        break;
+
+      case QuestionType.text:
+        return CupertinoTextField(
+          placeholder: widget.question.title,
+          autofocus: true,
+          controller: TextEditingController(text: widget.question.key),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          onSubmitted: (String value) {
+            Navigator.of(context).pop(value);
+          },
+        );
+
+      default:
+        return Text('Unbekannter Fragentyp');
+    }
+    return Container(); // Fallback für nicht behandelte Fälle
   }
 }
