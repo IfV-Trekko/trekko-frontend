cc3befe8f8bfb8a0fc028b39e1bb347bc978dd78
davidsteimel <132820861+davidsteimel@users.noreply.github.com>
2024-02-01 20:47:24 +0100

    feat: implement different types of diagrams  (#19)
    * feat: implement piechart and barchart
    
    * feat: completion of barchart and basicchart
    
    * fix: padding of analysis and page control

--- lib/screens/analysis/analysis.dart
+++ lib/screens/analysis/analysis.dart
@@ -1,17 +1,12 @@
-import 'package:app_backend/controller/analysis/reductions.dart';
+
 import 'package:app_backend/controller/trekko.dart';
-import 'package:app_backend/model/trip/leg.dart';
 import 'package:app_backend/model/trip/transport_type.dart';
-import 'package:app_backend/model/trip/trip.dart';
 import 'package:app_frontend/app_theme.dart';
 import 'package:app_frontend/components/page_control.dart';
-import 'package:app_frontend/screens/analysis/barchart.dart';
 import 'package:app_frontend/screens/analysis/piechart.dart';
 import 'package:app_frontend/screens/analysis/vehicle_data.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:fling_units/fling_units.dart';
-import 'package:isar/isar.dart';
-
+import 'barchart.dart';
 import 'basic_chart.dart';
 
 class Analysis extends StatefulWidget {
@@ -23,32 +18,10 @@ class Analysis extends StatefulWidget {
   _AnalysisState createState() => _AnalysisState();
 }
 
-// enum TransportTypeTheme {
-//   by_foot(TransportType.by_foot, AppThemeColors.turquoise),
-//   car(TransportType.car, AppThemeColors.orange),
-//   bicycle(TransportType.by_foot, AppThemeColors.purple),
-//   publicTransport(TransportType.by_foot, AppThemeColors.blue);
-//
-//   final TransportType type;
-//   final Color color;
-//
-//   const TransportTypeTheme(this.type, this.color);
-//
-//   static TransportTypeTheme? fromTransportType(TransportType type) {
-//     for (TransportTypeTheme theme in TransportTypeTheme.values) {
-//       if (theme.type == type) {
-//         return theme;
-//       }
-//     }
-//     return null;
-//   }
-// }
-
 class _AnalysisState extends State<Analysis> with AutomaticKeepAliveClientMixin{
 
   @override
   bool get wantKeepAlive => true;
-
 
   @override
   Widget build(BuildContext context) {
@@ -68,22 +41,22 @@ class _AnalysisState extends State<Analysis> with AutomaticKeepAliveClientMixin{
             SliverToBoxAdapter(
             child: CustomPageControl(
               pages: vehicleData,
+              pageHeights: 288,
             ),
             ),
             SliverToBoxAdapter(
-              child: Container(
-                margin: EdgeInsets.only(top: 14.0, left: 16.0, right: 16.0),
-                decoration: BoxDecoration(
-                  color: AppThemeColors.contrast0,
-                  border: Border.all(color: AppThemeColors.contrast400),
-                  borderRadius: BorderRadius.circular(8),
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 100.0),
+                child: CustomPageControl(
+                  pages: [
+                    PieChartWidget(trekko: widget.trekko),
+                    BarChartWidget(trekko: widget.trekko),
+                    BasicChart(trekko: widget.trekko),
+                  ],
+                  pageHeights: 440,
                 ),
-                padding: EdgeInsets.only(top: 9, bottom: 9, left: 12, right: 12),
-                child: PieChartWidget(trekko: widget.trekko),
-                //BarChartWidget(trekko: widget.trekko),
-                //BasicChart(trekko: widget.trekko),
               ),
-            )
+            ),
           ],
         )
     );
--- lib/screens/analysis/basic_chart.dart
+++ lib/screens/analysis/basic_chart.dart
@@ -3,54 +3,74 @@ import 'package:app_backend/controller/trekko.dart';
 import 'package:app_backend/model/trip/leg.dart';
 import 'package:app_backend/model/trip/transport_type.dart';
 import 'package:app_backend/model/trip/trip.dart';
-import 'package:app_frontend/screens/analysis/attribute_row.dart';
-import 'package:app_frontend/screens/journal/journalDetail/transportDesign.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:isar/isar.dart';
 import 'package:fling_units/fling_units.dart';
 
 import '../../app_theme.dart';
+import 'basicchart_row.dart';
 
-class BasicChart extends StatelessWidget{
-
+class BasicChart extends StatelessWidget {
   Trekko trekko;
-  //TransportType vehicle;
 
   BasicChart({required this.trekko});
 
-/*  Stream<SpeedReduction?> getData(TransportType vehicle) {
+  Stream<DerivedMeasurement<Measurement<Distance>, Measurement<Time>>?> getData(
+      TransportType vehicle) {
     return trekko.analyze(
         trekko
             .getTripQuery()
             .filter()
             .legsElement((l) => l.transportTypeEqualTo(vehicle))
             .build(),
-            (t) => t.getSpeed(),
-        SpeedReduction.AVERAGE
-    );
-  }*/
+        (t) => t.getSpeed(),
+        SpeedReduction.AVERAGE);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.only( top: 3, left: 4),
-        child:
-        Column(
-          children: [
-            Row(
-              children:[
-                Text('Geschwindigkeit',
+      decoration: BoxDecoration(
+        color: AppThemeColors.contrast0,
+        border: Border.all(color: AppThemeColors.contrast400),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      padding: EdgeInsets.only(top: 9, bottom: 9, left: 12, right: 12),
+      child: Container(
+          margin: EdgeInsets.only(top: 7, left: 4, right: 4),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(children: [
+                Text(
+                  'Ø Geschwindigkeit',
                   style: AppThemeTextStyles.title,
                 ),
-              ]
-            ),
-            SizedBox(height: 16),
-            // AttributeRow(title: TransportDesign.getName(vehicle),
-            //     value: getDataFormatted((t) => t.getDistance(),
-            //         DistanceReduction.AVERAGE, (d) => d.as(kilo.meters).roundToDouble().toString() + " km"))
-          ],
-        )
+              ]),
+              SizedBox(height: 16),
+              ...TransportType.values.map((type) {
+                return StreamBuilder<
+                    DerivedMeasurement<Measurement<Distance>,
+                        Measurement<Time>>?>(
+                  stream: getData(type),
+                  builder: (context, snapshot) {
+                    Widget dataWidget;
+                    if (snapshot.hasData) {
+                      var speed = snapshot.data
+                          ?.as(kilo.meters, hours)
+                          .roundToDouble()
+                          .toString(); // Ersetzen Sie dies durch die richtige Berechnung
+                      dataWidget = Text('${speed.toString()} km/h');
+                    } else {
+                      dataWidget = Text('Keine Daten');
+                    }
+                    return BasicChartRow(type: type, value: dataWidget);
+                  },
+                );
+              }).toList(),
+            ],
+          )),
     );
   }
 }
-
