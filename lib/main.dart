4aa38f70888ab268325226ae9d0da3dcc8f1d039
CuukyOfficial <just.cookie.jc@gmail.com>
2024-02-04 15:58:01 +0100

    refactor: remove unused method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
-import 'dart:io';
-
-import 'package:app_backend/controller/builder/build_exception.dart';
 import 'package:app_backend/controller/builder/last_login_builder.dart';
-import 'package:app_backend/controller/builder/login_builder.dart';
-import 'package:app_backend/controller/builder/login_result.dart';
-import 'package:app_backend/controller/builder/registration_builder.dart';
 import 'package:app_backend/controller/trekko.dart';
 import 'package:app_frontend/login/login_app.dart';
 import 'package:app_frontend/screens/trekko_app.dart';
@@ -28,35 +22,4 @@ void runTrekkoApp(Trekko trekko) {
 
 void runLoginApp() {
   runApp(LoginApp((trekko) => runTrekkoApp(trekko)));
-}
-
-Future<Trekko> buildTrekko() async {
-  late String ip;
-  if (Platform.isAndroid) {
-    ip = "10.0.2.2";
-  } else {
-    ip = "localhost";
-  }
-  try {
-    return await LoginBuilder(
-            "http://$ip:8080", "realAccount1@web.de", "1aA!hklj32r4hkjl324ra")
-        .build();
-  } catch (e) {
-    if (e is BuildException) {
-      if (e.reason == LoginResult.failedNoSuchUser) {
-        try {
-          return await RegistrationBuilder(
-                  "http://$ip:8080",
-                  "realAccount1@web.de",
-                  "1aA!hklj32r4hkjl324ra",
-                  "1aA!hklj32r4hkjl324ra",
-                  "12345")
-              .build();
-        } catch (e) {
-          print((e as BuildException).reason);
-        }
-      }
-    }
-    rethrow;
-  }
 }
