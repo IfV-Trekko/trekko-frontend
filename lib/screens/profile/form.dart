891b267a81ee073a7c11c319298375c7bdeb0dcf
CuukyOfficial <just.cookie.jc@gmail.com>
2024-02-07 16:30:25 +0100

    fix: form showing unparsed answer

--- lib/screens/profile/form.dart
+++ lib/screens/profile/form.dart
@@ -7,25 +7,24 @@ import 'package:app_backend/model/profile/question_type.dart';
 import 'package:app_frontend/app_theme.dart';
 import 'input_text_screen.dart';
 
-
 class QuestionTilesBuilder {
-
   static List<CupertinoListTile> buildQuestionTiles({
     required BuildContext context,
     required Profile profile,
     required Trekko trekko,
     required EdgeInsetsGeometry padding,
   }) {
-
     Future<void> showSelectPicker({
       required BuildContext context,
       required Profile profile,
       required Trekko trekko,
       required OnboardingQuestion question,
     }) async {
-      List<Widget> optionsWidgets = question.options!.map((option) => Center(
-        child: Text(option.answer),
-      )).toList();
+      List<Widget> optionsWidgets = question.options!
+          .map((option) => Center(
+                child: Text(option.answer),
+              ))
+          .toList();
 
       void onSettingSelected(int selectedIndex) {
         String selectedOptionKey = question.options![selectedIndex].key;
@@ -48,8 +47,8 @@ class QuestionTilesBuilder {
         OnboardingQuestion question, Function(String) onChange) async {
       final result = await Navigator.of(context).push(
         CupertinoPageRoute<String>(
-          builder: (BuildContext context) => TextInputPage(question: question,
-              trekko: trekko, profile: profile),
+          builder: (BuildContext context) => TextInputPage(
+              question: question, trekko: trekko, profile: profile),
         ),
       );
       if (result != null) {
@@ -60,34 +59,32 @@ class QuestionTilesBuilder {
 
     List<CupertinoListTile> questionTiles = [];
     for (OnboardingQuestion question
-    in profile.preferences.onboardingQuestions) {
-      dynamic answer =
-      profile.preferences.getQuestionAnswer(question.key);
+        in profile.preferences.onboardingQuestions) {
+      dynamic answer = profile.preferences.getQuestionAnswer(question.key);
 
       CupertinoListTile questionTile;
       if (question.type == QuestionType.boolean) {
         questionTile = CupertinoListTile.notched(
           padding: padding,
-          title: Text(question.title,
-              style: AppThemeTextStyles.normal),
+          title: Text(question.title, style: AppThemeTextStyles.normal),
           trailing: CupertinoSwitch(
             value: answer == null ? false : answer as bool,
             activeColor: AppThemeColors.green,
             onChanged: (bool? newValue) {
               if (newValue != null) {
-                profile.preferences.setQuestionAnswer(question.key,
-                    newValue);
+                profile.preferences.setQuestionAnswer(question.key, newValue);
                 trekko.savePreferences(profile.preferences);
               }
             },
           ),
         );
-      } else if (question.type == QuestionType.select) { //TODO: Answer parsing
+      } else if (question.type == QuestionType.select) {
         questionTile = CupertinoListTile.notched(
           padding: padding,
-          title: Text(question.title,
-              style: AppThemeTextStyles.normal),
-          additionalInfo: Text(answer ?? "Nicht beantwortet"),
+          title: Text(question.title, style: AppThemeTextStyles.normal),
+          additionalInfo: Text(answer != null
+              ? question.options!.where((e) => e.key == answer).first.answer
+              : "Nicht beantwortet"),
           trailing: const CupertinoListTileChevron(),
           onTap: () async {
             await showSelectPicker(
@@ -101,17 +98,17 @@ class QuestionTilesBuilder {
       } else {
         questionTile = CupertinoListTile.notched(
           padding: padding,
-          title: Text(question.title,
-              style: AppThemeTextStyles.normal),
+          title: Text(question.title, style: AppThemeTextStyles.normal),
           additionalInfo: Text(answer == null
               ? "Nicht beantwortet"
-              : question.type == QuestionType.number ? answer.toInt().toString() : answer.toString()),
+              : question.type == QuestionType.number
+                  ? answer.toInt().toString()
+                  : answer.toString()),
           trailing: const CupertinoListTileChevron(),
-          onTap: () => navigateAndEditText(profile, question,
-                  (String newValue) {
-                profile.preferences.setQuestionAnswer(question.key,
-                    newValue);
-              }),
+          onTap: () =>
+              navigateAndEditText(profile, question, (String newValue) {
+            profile.preferences.setQuestionAnswer(question.key, newValue);
+          }),
         );
       }
       questionTiles.add(questionTile);
@@ -119,4 +116,3 @@ class QuestionTilesBuilder {
     return questionTiles;
   }
 }
-
