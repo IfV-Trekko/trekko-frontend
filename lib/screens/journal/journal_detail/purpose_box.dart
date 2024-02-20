12eb5e5889dbff1947960fd9367e0dd8eb02a6aa
julian-rri <122223400+julian-rri@users.noreply.github.com>
2024-02-20 16:20:41 +0100

    fix/prupose_too_big (#54)

--- lib/screens/journal/journal_detail/purpose_box.dart
+++ lib/screens/journal/journal_detail/purpose_box.dart
@@ -7,23 +7,27 @@ class PurposeBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 100,
+      ),
       height: 24,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(4.0),
         border: Border.all(color: AppThemeColors.contrast200),
       ),
       padding: const EdgeInsets.symmetric(horizontal: 6),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 100),
-          child: Text(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
             _title,
             overflow: TextOverflow.ellipsis,
             style: AppThemeTextStyles.small.copyWith(
               color: AppThemeColors.contrast900,
             ),
           ),
-        ),
+        ],
       ),
     );
   }
