b7f0594920bacf6b9e4d97fe2ac24bfd4039fab4
davidsteimel <132820861+davidsteimel@users.noreply.github.com>
2024-03-14 19:39:30 +0100

    test: donate and delte test

---
+++ integration_test/donation_integration_test.dart
@@ -0,0 +1,58 @@
+import 'package:app_backend/controller/trekko.dart';
+import 'package:app_frontend/components/button.dart';
+import 'package:app_frontend/components/constants/button_style.dart';
+import 'package:app_frontend/screens/analysis/analysis.dart';
+import 'package:app_frontend/screens/journal/journal.dart';
+import 'package:app_frontend/screens/journal/journal_entry.dart';
+import 'package:app_frontend/screens/journal/journal_entry_detail_view/journal_entry_detail_view.dart';
+import 'package:app_frontend/screens/tracking/tracking.dart';
+import 'package:app_frontend/screens/trekko_app.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import '../test/test_utils.dart';
+
+void main () async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  Trekko? trekko;
+
+  tearDown(() async {
+    if (trekko != null) await TestUtils.clearTrekko(trekko!);
+  });
+
+  testWidgets('Test every screen, after onboarding',
+          (WidgetTester tester) async {
+        initializeDateFormatting();
+
+        final Trekko trekko = await TestUtils.initTrekko();
+        await tester.pumpWidget(TrekkoApp(trekko: trekko,),);
+
+        await tester.pumpAndSettle();
+        expect(find.byType(TrackingScreen), findsOneWidget);
+
+        await tester.tap(find.text('Tagebuch'));
+        await tester.pumpAndSettle(Duration(seconds: 5));
+        expect(find.byType(JournalScreen), findsOneWidget);
+        await tester.tap(find.widgetWithIcon(GestureDetector, CupertinoIcons.add));
+        await tester.pumpAndSettle();
+        expect(find.byType(JournalEntryDetailView), findsOneWidget);
+
+        await tester.tap(find.widgetWithText(Button, 'Spenden'));
+        await tester.pump(Duration(seconds: 5));
+        expect(find.text('Spende zurückziehen'), findsOneWidget);
+
+        await tester.tap(find.text('Statistik'));
+        await tester.pumpAndSettle();
+        expect(find.byType(Analysis), findsOneWidget);
+
+        await tester.tap(find.text('Tagebuch'));
+        await tester.pumpAndSettle();
+        expect(find.byType(JournalEntryDetailView), findsOneWidget);
+        await tester.tap(find.byWidgetPredicate((Widget button) => button is Button && button.style == ButtonStyle.destructive));
+        await tester.pumpAndSettle();
+        await tester.tap(find.text('Löschen'));
+        expect(find.byType(JournalEntry), findsNothing);
+      });
+}
