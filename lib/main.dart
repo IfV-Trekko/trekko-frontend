7fd90abf37c1c9558b67741290fc622234d67c80
CuukyOfficial <just.cookie.jc@gmail.com>
2024-03-18 14:26:41 +0100

    feat: transparent ui overlay

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:app_backend/controller/builder/last_login_builder.dart';
 import 'package:app_backend/controller/trekko.dart';
 import 'package:app_frontend/screens/onboarding/login_app.dart';
 import 'package:app_frontend/screens/trekko_app.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
@@ -10,7 +10,10 @@ void main() async {
   await initializeDateFormatting('de', null);
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: Colors.transparent,
+  ));
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
